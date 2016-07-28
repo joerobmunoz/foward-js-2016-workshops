482353de19ff81e6bd2e193ddbd47229ad627793
Joe Munoz <joe.rob.munoz@gmail.com>
2016-07-28 12:17:54 -0700

    exercise 1 and 2.

--- async/exercises/ex1/ex1.js
+++ async/exercises/ex1/ex1.js
@@ -20,9 +20,48 @@ function output(text) {
 // **************************************
 // The old-n-busted callback way
 
+var responses = { }
+
+function setFired(file) {
+	output(file)
+	responses[file] = "fired"
+}
+
+function firePrevious(file) {
+	// check if previous dictionary key exists and is not "fired", then fire previous
+	var previousFileName = "file" + (file.split('').pop()-1).toString();
+	// output('fire previous ' + previousFileName)
+	if (previousFileName in responses) {
+		if (responses[previousFileName] != "fired") {
+			setFired(previousFileName)
+		}
+
+		fireNext(previousFileName)
+	}
+}
+
+function fireNext(file) {
+	// console.log('fire next file: ' + file)
+	var next = 'file' + (parseInt(file.split('').pop())+1).toString();
+	if ((next in responses)) {
+		setFired(next)
+		fireNext(next)
+	}
+}
+
 function getFile(file) {
-	fakeAjax(file,function(text){
+	fakeAjax(file, function(text) {
 		// what do we do here?
+		if (responses[file] != "fired") {
+			responses[file] = text 
+		}
+
+		if (file.split('').pop() == 1) {
+			setFired(file)
+			fireNext(file)
+		} else {
+			firePrevious(file)
+		}
 	});
 }
 
--- async/exercises/ex2/ex2.js
+++ async/exercises/ex2/ex2.js
@@ -21,7 +21,40 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	var response,
+		fn;
+
+	// Both code blocks below can occur
+		// Add conditionals to care both either case occuring first
+		// keep a reference to the sibling event in the function scope
+	fakeAjax(file, function(resp) {
+		response = resp
+		if (fn)  fn(response)
+	});
+
+	return function thunk(cb) {
+		fn = cb
+		if (response) fn(response)
+	}
 }
 
 // request all files at once in "parallel"
 // ???
+
+
+var th1 = getFile('file1');
+var th2 = getFile('file2');
+var th3 = getFile('file3');
+
+
+// We don't want to think about 'time', so encapsulate it in thunks
+th1(function(text1) {
+	output(text1);
+	th2(function(text2) {
+		output(text2);
+		th3(function(text3) {
+			output(text3);
+			output("Complete!");
+		})
+	})
+})
